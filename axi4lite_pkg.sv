6c679fe9abeb733f237ca86b03d745b7da8493b6
AppleWaffleZ <mhd4niall2102@gmail.com>
2026-07-16 10:09:45 +0800

    Add axi4lite_pkg.sv with UVM includes

---
+++ axi4lite_pkg.sv
@@ -0,0 +1,13 @@
+// axi4lite_pkg.sv
+
+`include "uvm_macros.svh"
+
+`include "sequence_item.sv"
+`include "sequencer.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "write_read_transaction_seq.sv"
+`include "agent.sv"
+`include "scoreboard.sv"
+`include "addr_coverage.sv"
+`include "env.sv"
